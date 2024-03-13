5afe436eb4553dc5b5ce1b8fed5c487723fbbc6c
masfaghori <161416552+masfaghori@users.noreply.github.com>
2024-03-13 19:39:47 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+// student2 
+var user = {
+    fullName: "Yamna",
+    religion: "Islam",
+    nationality: "Pakistani",
+    rollNumber: 30,
+    email: "yamnagmail@.com",
+    contact: 3316982579,
+    age: 21,
+    isPassed: true,
+    status: "unmarried",
+    degree: ["graduate", "masters"],
+    subjects: ["english", "urdu", "Programing"],
+    feeschudele: {
+        month: "March,2024",
+        amount: 10000,
+        }
+};
+console.log(user.fullName);
+console.log(user.nationality);
+console.log(user.age);
+console.log(user.subjects[2]);
+console.log(user.email);
+console.log(user.contact);
+console.log(user.feeschudele.month + ", amount is 10000");
+// {key: "a", key2: [] }
+//let array = [{}, {}, {}, [], [], true, "a"]
+//let obj ={
+//   key:{
+//       key:{}
+// };
+//  key2:[]
+//}
